19a292d16be2d21bbf01e52c2eef7db6de6f935b
Vanessa Power <vanessa.l.power@gmail.com>
2022-08-10 13:08:35 -0700

    writing the assert if arrays are equal function

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+const eqArrays = (arr1, arr2) => {
+  const str1 = arr1
+    .map((el) => (typeof el === "string" ? `S${el}` : `N${el}`))
+    .join("");
+  const str2 = arr2
+    .map((el) => (typeof el === "string" ? `S${el}` : `N${el}`))
+    .join("");
+  return str1 === str2;
+};
+const assertArraysEqual = (arr1, arr2) => {
+  eqArrays(arr1, arr2)
+    ? console.log("The arrays are equal!")
+    : console.log("The arrays are different.");
+};
+
+//Test cases
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
