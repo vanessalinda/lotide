2d74ea326fc6d6efded309897d9eb0eba282d181
Vanessa Power <vanessa.l.power@gmail.com>
2022-08-09 20:24:56 -0700

    updating the emojis

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,11 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = (actual, expected) => {
   if (actual === expected)
-    console.log(`🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   if (actual !== expected)
-    console.log(`✅✅✅ Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+// // TEST CODE
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
