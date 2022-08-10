49dc25987edacbabe6208488111c31df4c1e37af
Vanessa Power <vanessa.l.power@gmail.com>
2022-08-09 18:51:23 -0700

    adding assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = (actual, expected) => {
+  if (actual === expected)
+    console.log(`🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`);
+  if (actual !== expected)
+    console.log(`✅✅✅ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
