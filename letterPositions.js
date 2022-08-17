2daecf780c3d9f14df400b84c632fe58158b9841
Vanessa Power <vanessa.l.power@gmail.com>
2022-08-16 17:52:08 -0700

    writing the letter positions function

---
+++ letterPositions.js
@@ -0,0 +1,29 @@
+const eqArrays = (arr1, arr2) => {
+  const str1 = arr1
+    .map((el) => (typeof el === "string" ? `S${el}` : `N${el}`))
+    .join("");
+  const str2 = arr2
+    .map((el) => (typeof el === "string" ? `S${el}` : `N${el}`))
+    .join("");
+  return str1 === str2;
+};
+const assertArraysEqual = (arr1, arr2) => {
+  eqArrays(arr1, arr2)
+    ? console.log("The arrays are equal!")
+    : console.log("The arrays are different.");
+};
+
+const letterPositions = (str) => {
+  const sentence = str.split("");
+  const result = sentence.reduce((acc, letter, index) => {
+    if (letter !== " ") {
+      acc[letter] = acc[letter] || [];
+      acc[letter].push(index);
+    }
+    return acc;
+  }, {});
+  return result;
+};
+
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("lighthouse in the house").h, [3, 5, 15, 18]);
