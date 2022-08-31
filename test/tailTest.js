24e88e2890f05e36f1ac84fec774de9f1e2c1142
Vanessa Power <vanessa.l.power@gmail.com>
2022-08-31 09:26:17 -0700

    refactoring tailTest for mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,31 @@
-const assertEqual = require("../assertEqual");
 const tail = require("../tail");
+const assert = require("chai").assert;
+//const assertEqual = require("../assertEqual")
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words), ["Lighthouse", "Labs"];
-//Testing the original array is not mutated
-assertEqual(words.length, 3);
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), [
+      "Lighthouse",
+      "Labs",
+    ]);
+  });
 
-const numbers = tail([1, 2, 3, 4, 5]);
-assertEqual(numbers.length, 4);
+  it("returns [2, 3, 4, 5] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
+  });
 
-const one = tail([1]);
-assertEqual(one.length, 0);
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+});
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words), ["Lighthouse", "Labs"];
+// //Testing the original array is not mutated
+// assertEqual(words.length, 3);
+
+// const numbers = tail([1, 2, 3, 4, 5]);
+// assertEqual(numbers.length, 4);
+
+// const one = tail([1]);
+// assertEqual(one.length, 0);
