9bbb1d52e7f51413b5a3888ce39dbc7aa413e3f6
Vanessa Power <vanessa.l.power@gmail.com>
2022-08-30 20:07:05 -0700

    updating assert arrays equal function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,9 @@
-const eqArrays = (arr1, arr2) => {
-  const str1 = arr1
-    .map((el) => (typeof el === "string" ? `S${el}` : `N${el}`))
-    .join("");
-  const str2 = arr2
-    .map((el) => (typeof el === "string" ? `S${el}` : `N${el}`))
-    .join("");
-  return str1 === str2;
-};
+const eqArrays = require("./eqArrays");
+
 const assertArraysEqual = (arr1, arr2) => {
   eqArrays(arr1, arr2)
     ? console.log("The arrays are equal!")
     : console.log("The arrays are different.");
 };
 
-//Test code
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArraysEqual;
